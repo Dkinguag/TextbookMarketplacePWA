1aa0e9a1fe997f05774a3cd03db852d51237eaf9
kingd <dkinguag@gmail.com>
2020-02-10 19:00:24 -0600

    InvalidHookCallError

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
         // The domain name of your Shopify shop. This is required.
         // Example: 'gatsby-source-shopify-test-shop' if your Shopify address is
         // 'gatsby-source-shopify-test-shop.myshopify.com'.
-        shopName: process.env.SHOP_NAME,
+        shopName: 'drewssenioryearflashsale',
 
         // An API access token to your Shopify shop. This is required.
         // You can generate an access token in the "Manage private apps" section
@@ -49,7 +49,7 @@ module.exports = {
         // to select "Allow this app to access your storefront data using the
         // Storefront API".
         // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
-        accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
+        accessToken: 'b6e4d3176a26ccd478a813fcb436db9e',
 
         // Set verbose to true to display a verbose output on `npm run develop`
         // or `npm run build`. This prints which nodes are being fetched and how
